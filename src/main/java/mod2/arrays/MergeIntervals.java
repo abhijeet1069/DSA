bd972dbf619fc559981b5a0db899d9614e8ff073
abhijeet1069 <abhijeet1069@gmail.com>
2022-09-28 13:49:01 +0530

    first

---
+++ src/main/java/mod2/arrays/MergeIntervals.java
@@ -0,0 +1,68 @@
+package mod2.arrays;
+
+// Importing required functions
+import java.beans.IntrospectionException;
+import java.util.*;
+class Interval {
+    public int first;
+    public int second;
+
+    public Interval(int x, int y) {
+        this.first = x;
+        this.second = y;
+    }
+}
+
+public class MergeIntervals {
+
+    public static boolean isMergePossible(Interval a, Interval b){
+        if(a.first <= b.second && b.first <= a.second)
+            return true;
+        else
+            return false;
+    }
+
+    public static Interval merge(Interval a, Interval b){
+        return new Interval(Math.min(a.first, b.first), Math.max(a.second, b.second));
+    }
+
+    static ArrayList<Interval> mergeIntervals(ArrayList<Interval> v) {
+        ArrayList<Interval> output = new ArrayList<Interval>();
+
+        if(v.size() <= 1)
+            return v;
+
+        Collections.sort(v, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval a, Interval b){
+                return Integer.compare(a.first, b.first);
+            }
+        });
+
+        Stack<Interval> stack = new Stack<>();
+        stack.push(v.get(0));
+        for(int i = 1; i < v.size(); i++){
+            if(isMergePossible(v.get(i), stack.peek()))
+                stack.push(merge(stack.pop(), v.get(i)));
+            else
+                stack.push(v.get(i));
+        }
+
+        while(!stack.empty())
+            output.add(0,stack.pop());
+
+        //for(Interval i : output)
+          //  System.out.println(i.first+","+i.second);
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Interval> input = new ArrayList<Interval>();
+        input.add(new Interval(1,3));
+        input.add(new Interval(2,6));
+        input.add(new Interval(8,10));
+        input.add(new Interval(15,18));
+        mergeIntervals(input);
+    }
+}
---
+++ src/main/java/mod2/common/SortBy2ndParam.java
@@ -0,0 +1,22 @@
+package mod2.common;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+* Sort array on basis of second Parameter.
+* */
+public class SortBy2ndParam {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{{1,2},{3,4},{2,4},{5,-1},{8,10},{1,2}};
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[1],o2[1]);
+            }
+        });
+        for(int[] temp : arr){
+            System.out.println(temp[0]+" "+temp[1]);
+        }
+    }
+}
