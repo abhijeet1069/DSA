6506de153c6c41e1072050392cedd51f52b2e1b6
abhijeet1069 <abhijeet1069@gmail.com>
2023-07-09 21:02:18 +0530

    new commit

--- Skiena/Algortihms/src/C1_Intro/InsertionSort.java
+++ Skiena/Algortihms/src/C1_Intro/InsertionSort.java
@@ -1,16 +1,29 @@
 package C1_Intro;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InsertionSort {
 	
-	public static void swap(int[] arr) {
-		int temp = arr[0];
-		arr[0] = arr[1];
-		arr[1] = temp;
+	public static void insertion_sort(int[] arr) {
+		int i,j,temp;
+		for(i = 1; i < arr.length; i++) {
+			j = i;
+			while((j > 0) && (arr[j] < arr[j-1])) { //this line prevents unnecessary comparisons.
+				temp = arr[j];                      //if elements are in ascending order then no need to check further on left
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				
+				j = j-1;
+			}
+		}
 	}
+
 	
 	public static void main(String[] args) {
-		int[] arr = {12,22};
-		swap(arr);
-		System.out.println("arr[0] : "+arr[0]+" arr[1] : "+arr[1]);
+		readCsv();
 	}
 }
