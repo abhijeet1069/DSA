2625f868f4a1028faf081b1856790f9cd4959ad0
abhijeet1069 <abhijeet1069@gmail.com>
2023-07-15 21:18:48 +0530

    new commit

--- CLRS/Algorithms/src/intro/InsertionSort.java
+++ CLRS/Algorithms/src/intro/InsertionSort.java
@@ -6,11 +6,11 @@ public class InsertionSort {
 		for(int i = 1; i < arr.length; i++) {
 			int key = arr[i];
 			int j = i-1;
-			while(j > 0 && arr[j] > key) {
+			while(j >= 0 && arr[j] < key) {
 				arr[j+1] = arr[j];
 				j = j-1;
-				arr[j+1] = key;
 			}
+			arr[j+1] = key;
 		}
 	}
 	
